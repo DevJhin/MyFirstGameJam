6365e4ed8e6d65febcc219f3a7c614d620bc9de9
UnrealChan <bomul0327@naver.com>
2021-01-10 05:19:18 +0900

    FieldObject클래스 추가

---
+++ Assets/Scripts/FieldObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 필드에 있을 수 있는 캐릭터, 기믹의 최상위 오브젝트
+/// </summary>
+public class FieldObject : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/FieldObjectBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 필드에 있을 수 있는 모든 것들의 행동 구현체
+/// </summary>
+public class FieldObjectBehaviour
+{
+   
+}
