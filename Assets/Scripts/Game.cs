6d51699a159c8d3df0aa45708b71d14574276d0e
UnrealChan <bomul0327@naver.com>
2021-01-10 05:18:59 +0900

    게임 전체 관리 클래스 및 스테이지 관리 클래스 추가

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 전반적인 게임의 흐름을 모두 제어하는 클래스 (게임이 켜지면 무조건 실행되는 녀석)
+/// </summary>
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Stage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 게임이 진행되는 스테이지
+/// 우리 같은 경우에는 실제 게임은 스테이지 내부에서만 진행되기 때문에 실제 게임의 객체들을 관리는 여기서 진행한다.
+/// </summary>
+public class Stage
+{
+    
+}
